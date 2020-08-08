fa1535792ce2b7ab3b7c71f8f921c7972bd7284a
RafaelAMello <raf@rafaels-mbp.lan>
2020-08-08 20:02:13 +1000

    change topic name

--- face-recognition-v3/send_data.py
+++ face-recognition-v3/send_data.py
@@ -4,4 +4,5 @@ from json import dumps
 producer = KafkaProducer(bootstrap_servers=['0.tcp.au.ngrok.io:10624'],
                          value_serializer=lambda x: dumps(x).encode('utf-8'))
 
-producer.send('test', value={'hello' : 'world'})
+
+producer.send('picture', value={'hello' : 'world'})
