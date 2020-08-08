884ef3efdba5557373b8e68455f08b341af83af2
RafaelAMello <raf@rafaels-mbp.lan>
2020-08-08 19:59:18 +1000

    Fix indentation

--- face-recognition-v3/send_data.py
+++ face-recognition-v3/send_data.py
@@ -1,5 +1,5 @@
- from kafka import KafkaProducer
- from json import dumps
+from kafka import KafkaProducer
+from json import dumps
 
 producer = KafkaProducer(bootstrap_servers=['0.tcp.au.ngrok.io:10624'],
                          value_serializer=lambda x: dumps(x).encode('utf-8'))
