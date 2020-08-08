5d11fe5dd683899e74f1dfff118aba5081917e33
RafaelAMello <raf@rafaels-mbp.lan>
2020-08-08 19:49:18 +1000

    Add third version

---
+++ face-recognition-v3/README.md
@@ -0,0 +1,2 @@
+ssh -N -L 0.0.0.0:8888:localhost:8080 -N 127.0.0.1
+~/ngrok tcp 9092
---
+++ face-recognition-v3/send_data.py
@@ -0,0 +1,7 @@
+ from kafka import KafkaProducer
+ from json import dumps
+
+producer = KafkaProducer(bootstrap_servers=['0.tcp.au.ngrok.io:10624'],
+                         value_serializer=lambda x: dumps(x).encode('utf-8'))
+
+producer.send('test', value={'hello' : 'world'})
