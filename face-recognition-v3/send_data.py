3910d9440d96d86f4be2832d5138f357c04da200
RafaelAMello <raf@rafaels-mbp.lan>
2020-08-08 22:11:39 +1000

    Send pictures

--- face-recognition-v3/send_data.py
+++ face-recognition-v3/send_data.py
@@ -1,8 +1,23 @@
 from kafka import KafkaProducer
-from json import dumps
+from datetime import datetime
+import os
 
-producer = KafkaProducer(bootstrap_servers=['0.tcp.au.ngrok.io:10624'],
-                         value_serializer=lambda x: dumps(x).encode('utf-8'))
+def take_picture():
+    picture_time = datetime.now()
+    picture_location = f"~/Desktop/{picture_time}.jpg"
+    os.system(f"fswebcam -r 1280x730 --no-banner {picture_location}")
+    return picture_location, picture_time
 
+def delete_picture(picture_location):
+    os.remove(picture_location)
 
-producer.send('picture', value={'hello' : 'world'})
+def read_picture_data(picture_location):
+    return read(picture_location, 'rb').read()
+
+producer = KafkaProducer(
+    bootstrap_servers=['0.tcp.au.ngrok.io:10504'])
+
+picture_location, picture_time = take_picture()
+picture_data = read_picture_data(picture_location)
+
+future = producer.send('picture', key=bytes(str(picture_time)), value=picture_data)
