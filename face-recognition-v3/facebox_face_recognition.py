991c70add2095edb9f48111e7a6647dc230042c0
RafaelAMello <raf@rafaelpersonal.lan>
2020-08-09 14:07:21 +1000

    Send Kafka Metrics

--- face-recognition-v3/facebox_face_recognition.py
+++ face-recognition-v3/facebox_face_recognition.py
@@ -1,4 +1,5 @@
 import requests
+from glob import glob
 from draw_face import initialize_draw, show_draw, draw_face, BLUE, RED
 
 def upload_picture(url, file_path):
@@ -12,17 +13,31 @@ def upload_picture(url, file_path):
 def process_faces(file_path):
     payload = upload_picture("http://localhost:8080/facebox/check", file_path)
     draw, pil_image = initialize_draw(file_path=file_path)
+    has_face = False
     for face in payload['faces']:
+        has_face = True
         print(payload)
         top = face['rect']['top']
         left = face['rect']['left']
         right = left + face['rect']['width']
         bottem = top + face['rect']['height']
-        color = BLUE if face['matched'] else RED
-        draw_face(draw, "rafael", color, top, right, bottem, left)
-    show_draw(draw, pil_image)
+        if face['matched']:
+            color = BLUE
+            name = face['name']
+        else:
+            color = RED
+            name = 'Unknown'
+        draw_face(draw, name, color, top, right, bottem, left)
+    if has_face:
+        show_draw(draw, pil_image)
+    return payload
 
-def process_image_tags(file_path):
-    payload = upload_picture("http://localhost:8080/facebox/check", file_path)
-
-
+def train_faces(person):
+    headers = {"Accept" : "application/json; charset=utf-8"}
+    for picture_path in glob(f"train/{person}/*.jpg"):
+        params = {'name' : person, 'id' : picture_path.split('/')[-1]}
+        files = {'file': open(picture_path,'rb')}
+        response = requests.post("http://localhost:8080/facebox/teach", params=params, headers=headers, files=files)
+        print(response)
+        assert response.status_code == 200, f"Response: {response.json()}, {picture_path}"
+        print(response.json())
--- face-recognition-v3/read_data.py
+++ face-recognition-v3/read_data.py
@@ -1,28 +1,40 @@
 import io
-from kafka import KafkaConsumer
+import json
+from kafka import KafkaConsumer, KafkaProducer
 from PIL import Image
 
-def download_faces(process_function):
-    consumer = KafkaConsumer(
-                'picture',
-                group_id='my-group',
-                bootstrap_servers='kafka-49b7861-rafaelathaydemello-3b01.aivencloud.com:25697',
-                security_protocol="SSL",
-                ssl_cafile="ca.pem",
-                ssl_certfile="service.cert",
-                ssl_keyfile="service.key"
+consumer = KafkaConsumer(
+            'picture',
+            group_id='my-group',
+            bootstrap_servers='kafka-49b7861-rafaelathaydemello-3b01.aivencloud.com:25697',
+            security_protocol="SSL",
+            ssl_cafile="ca.pem",
+            ssl_certfile="service.cert",
+            ssl_keyfile="service.key"
+)
+
+producer = KafkaProducer(
+    bootstrap_servers='kafka-49b7861-rafaelathaydemello-3b01.aivencloud.com:25697',
+    security_protocol="SSL",
+    ssl_cafile="ca.pem",
+    ssl_certfile="service.cert",
+    ssl_keyfile="service.key",
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')
     )
+
+def download_faces(process_function, send_kafka_payload=False):
     for message in consumer:
         print(f"{message.topic}:{message.partition}:{message.offset}: key={message.key}")
         image = Image.open(io.BytesIO(message.value))
         file_name = "test.jpg"
         image.save(file_name)
-        process_function(file_name)
+        if send_kafka_payload:
+            payload = process_function(file_name)
+            picture_time = message.key
+            producer.send('picture-metadata', key=picture_time, value=payload)
+        else:
+            process_function(file_name)
 
 if __name__ == "__main__":
-    # pass
-    # from python_face_recognition import process_face
-    # download_faces(process_face)
-
     from facebox_face_recognition import process_faces
-    download_faces(process_faces)
+    download_faces(process_faces, send_kafka_payload=True)
