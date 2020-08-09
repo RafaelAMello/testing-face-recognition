fe82c1e26f396c2fdc230dbe34dc55f2b492d3c8
RafaelAMello <raf@rafaelpersonal.lan>
2020-08-09 12:52:43 +1000

    change send data

--- face-recognition-v3/send_data.py
+++ face-recognition-v3/send_data.py
@@ -6,7 +6,7 @@ def take_picture():
     picture_time = datetime.now()
     picture_location = f"{picture_time}.jpg".replace(' ', '')
     # https://lbhtran.github.io/Camera-setting-and-photo-taking-schedule-to-get-the-best-result/
-    # fswebcam -D 2 -S 20 --set brightness=30% --set contrast=0%  -F 10 -r  640x480 --no-banner /home/pi/camera/$DATE.jpg
+    # fswebcam -D 2 -S 20 --set Focus, Auto=False --set brightness=30% --set contrast=0%  -F 10 -r  640x480 --no-banner /home/pi/camera/$DATE.jpg
 
     os.system(f"fswebcam -S 50 -r 1280x730 --no-banner {picture_location}")
     return picture_location, picture_time
