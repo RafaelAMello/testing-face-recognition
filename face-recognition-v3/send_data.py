39fd547d44b479757fae0a8816d7c1fcc7c7f283
RafaelAMello <raf@rafaelpersonal.lan>
2020-08-11 09:44:58 +1000

    Properly import enviroment variables

--- face-recognition-v3/send_data.py
+++ face-recognition-v3/send_data.py
@@ -1,13 +1,12 @@
 import os
-from os.path import join, dirname
 from datetime import datetime
 
 from dotenv import load_dotenv
 from kafka import KafkaProducer
 
-BOOTSTRAP_SERVER = f"{os.getenv('KAFKA_SERVER_HOST')}:{os.getenv('KAFKA_SERVER_PORT')}"
-dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
+
+BOOTSTRAP_SERVER = f"{os.getenv('KAFKA_SERVER_HOST')}:{os.getenv('KAFKA_SERVER_PORT')}"
 
 def take_picture():
     picture_time = datetime.now()
