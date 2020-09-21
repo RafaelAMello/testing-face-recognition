ccf3406183206fe9a90335f7f2b8e01c4737cdfb
RafaelAMello <raf@rafaelpersonal.lan>
2020-09-21 16:09:00 +1000

    Handle Termination

--- give_conn.py
+++ give_conn.py
@@ -6,17 +6,25 @@ from dotenv import load_dotenv
 
 load_dotenv('.env')
 r = redis.from_url(os.environ['REDIS_URL'])
+try:
+    while True:
+        if r.get('connection_delete_requested') is not None:
+            print("Handling Delete")
+            ngrok.kill()
+            r.delete('conn_url')
+            r.delete('connection_delete_requested')
 
-while True:
-    if r.get('connection_delete_requested') is not None:
-        print("Handling Delete")
-        ngrok.kill()
-        r.delete('conn_url')
-        r.delete('connection_delete_requested')
+        if r.get('connection_requested') is not None:
+            ngrok.kill()
+            print("Handling Create")
+            ssh_url = ngrok.connect(22, "tcp")
+            r.set('conn_url', ssh_url)
+            r.delete('connection_requested')
 
-    if r.get('connection_requested') is not None:
-        ngrok.kill()
-        print("Handling Create")
-        ssh_url = ngrok.connect(22, "tcp")
-        r.set('conn_url', ssh_url)
-        r.delete('connection_requested')
+except KeyboardInterrupt as err:
+    print()
+    print("Terminating")
+    ngrok.kill()
+    r.delete('conn_url')
+    r.delete('connection_delete_requested')
+    r.delete('connection_requested')
