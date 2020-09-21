aa5e3deb80a33a9b1ae8b739910c298256394b6e
RafaelAMello <raf@rafaelpersonal.lan>
2020-09-21 10:52:40 +1000

    Add connections

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
+import os
 from flask import Flask
+from flask import jsonify
+import redis
 
 app = Flask(__name__)
+PASSWORD =  os.environ['PASSWORD']
+r = redis.from_url(os.environ['REDIS_URL'])
 
-@app.route('/')
-def hello():
-    return 'hello'
+@app.route('/<password>', methods=['GET'])
+def get_con(password):
+    if PASSWORD == password:
+        r.set('connection_requested', 'yes')
+        while r.get('conn_url') is None:
+            pass
+        return jsonify({'conn_url' : r.get('conn_url').decode()})
+    return password
+
+@app.route('/<password>', methods=['DELETE'])
+def delete_con(password):
+    if PASSWORD == password:
+        if r.get('conn_url') is None:
+            return jsonify({'done' : False})
+        r.set('connection_delete_requested', 'yes')
+        while r.get('conn_url') is not None:
+            pass
+        return jsonify({'done' : True})
+    return password
 
 if __name__ == "__main__":
     app.run()
---
+++ get_connection.py
@@ -0,0 +1,22 @@
+import os
+import redis
+from time import sleep
+from pyngrok import ngrok
+from dotenv import load_dotenv
+
+load_dotenv('.env')
+r = redis.from_url(os.environ['REDIS_URL'])
+
+while True:
+    if r.get('connection_delete_requested') is not None:
+        print("Handling Delete")
+        ngrok.kill()
+        r.delete('conn_url')
+        r.delete('connection_delete_requested')
+
+    if r.get('connection_requested') is not None:
+        print("Handling Create")
+        ssh_url = ngrok.connect(22, "tcp")
+        r.set('conn_url', ssh_url)
+        r.delete('connection_requested')
+    
